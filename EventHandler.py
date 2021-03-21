cf1f7a9bd223708577763cc5c63df9669c6df2b4
Chris Chuang <67641657+cchuan3@users.noreply.github.com>
2021-03-21 10:59:35 -0500

    Upload EventHandler class
    - Class that stores eventData objects and allows searching through the list
    - Search types implemented: substring in event name, date (and, optionally, time)

--- EventHandler.py
+++ EventHandler.py
@@ -1,95 +1,32 @@
-# Event Handler
-from RSVPHandler import *
-from EventData import *
+# Event Handler: Stores event data and allows searching for events
+from EventIO import EventIO
+from EventData import EventData
 
 class EventHandler:
     def __init__(self):
-        self.eventNames = []
         self.events = []
-        self.RSVPHandler = RSVPHandler()
     
-    # Return names of all events
+    # Return all events
     def getAllEvents(self):
-        return self.eventNames
+        return self.events
 
-    # Return dictionary of {eventName:time}
-    def getAllEventTimes(self):
-        times = {}
+    # Return events matching given name (If the name is a substring of the event name)
+    def searchName(self, name):
+        retEvents = []
         for event in self.events:
-            times.update({event.getName():event.getTime()})
-        return times
+            if name in event.getName():
+                retEvents.append(event)
+        return retEvents
 
-    # Return dictionary of {eventName:[tags]}
-    def getAllEventTags(self):
-        tags = {}
-        for event in self.events:
-            tags.update({event.getName():event.getTags()})
-        return tags
-
-    # Return dictionary of {eventName:[RSVPList]}
-    def getAllRSVPLists(self):
-        allRSVPLists = {}
-        for event in self.events:
-            allRSVPLists.update({event.getName():event.getRSVPList()})
-        return allRSVPLists
-    
-    # Load events to memory (EventData objects in self.events)
-    def loadEvents(self, names, times, tags):
-        self.eventNames = names
-        for eventName in names:
-            self.events.append(EventData(eventName, times[eventName], tags[eventName]))
-    
-    # Load RSVPLists to event objects
-    def loadRSVPLists(self, listData):
-        self.RSVPHandler.setLists(listData)
-        for event in self.events:
-            event.loadRSVPList(self.RSVPHandler.loadList(event.getName()))
-    
-    # Add user to event RSVPList
-    def addRSVP(self, eventName, userName):
-        if not eventName in self.eventNames:
-            print("Event not found")
-            return False
+    # Return all events at given date/time (time not necessary)
+    def searchDate(self,date, time=None):
+        retEvents = []
+        if (time == None):
+            for event in self.events:
+                if (event.getDate() == date):
+                    retEvents.append(event)
         else:
             for event in self.events:
-                if event.getName() == eventName:
-                    return event.addRSVP(userName)
-    
-    # Remove user from event RSVPList
-    def removeRSVP(self, eventName, userName):
-        if not eventName in self.eventNames:
-            print("Event not found")
-            return False
-        else:
-            for event in self.events:
-                if event.getName() == eventName:
-                    return event.removeRSVP(userName)
-    
-    # Create a new event
-    def addEvent(self, name, time, tags, user):
-        if (name in self.eventNames):
-            print(f"Event ({name}) already exists")
-            return False
-        else:
-            tagList = []
-            tags = tags.split()
-            # Replace "_" with " " ("_" is in the file so that tags with " " are still read as one tag)
-            for tag in tags:
-                tag = tag.replace('_', ' ')
-                # Check if a tag is duplicated for the event
-                if tag in tagList:
-                    print(f"Error: {name} has duplicate tag \"{tag}\" in new event creation")
-                    return False
-                tagList.append(tag)
-
-            self.eventNames.append(name)
-            newEvent = EventData(name, time, tags)
-            newEvent.addRSVP(user)
-            self.events.append(newEvent)
-            print(f"Successfully created {name} event")
-            return True
-
-    # Printing for debugging
-    def printAllEvents(self):
-        for event in self.events:
-            event.printAllData()
+                if (event.getDate() == date and event.getTime() == time):
+                    retEvents.append(event)
+        return retEvents
